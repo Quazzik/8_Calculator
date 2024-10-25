b9a9a274e48fc34ea922ce1ed9768bc5447d25f4
Quazzik <yuakorotkov@gmail.com>
2024-10-25 19:03:23 +0500

    Small patch with fixes
    Fixed incorrect path to access controller, fixed controller name, added button to delete old records in database

--- 8_Calculator/8_Calculator/Controllers/HomeController.cs
+++ 8_Calculator/8_Calculator/Controllers/HomeController.cs
@@ -1,32 +1,87 @@
+using _8_Calculator.DB.Entities;
+using _8_Calculator.Enums;
 using _8_Calculator.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
+using static _8_Calculator.Enums.OperationEnum;
 
 namespace _8_Calculator.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly BatabaseContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(BatabaseContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
+            SetOldResults();
             return View();
         }
 
-        public IActionResult Privacy()
+        [HttpPost]
+        public IActionResult Delete(int Id)
         {
-            return View();
+            var calculation = _context.Calculations.Find(Id);
+
+            if (calculation != null)
+            {
+                _context.Calculations.Remove(calculation);
+                _context.SaveChanges();
+            }
+            SetOldResults();
+            return View("Index");
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Calculate(double num1, double num2, OperationType operation)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            double result = 0;
+            switch (operation)
+            {
+                case OperationType.Add:
+                    result = num1 + num2;
+                    break;
+                case OperationType.Subtract:
+                    result = num1 - num2;
+                    break;
+                case OperationType.Multiply:
+                    result = num1 * num2;
+                    break;
+                case OperationType.Divide:
+                    result = num1 / num2;
+                    break;
+            }
+            ViewBag.Result = result;
+
+            var calc = new Calculation
+            {
+                Operand1 = num1,
+                Operand2 = num2,
+                Result = result,
+                Operation = operation.ToString()
+            };
+            _context.Calculations.Add(calc);
+            _context.SaveChanges();
+
+            SetOldResults();
+
+            return View("Index");
+        }
+        public void SetOldResults()
+        {
+            var calculations = _context.Calculations.ToList();
+            foreach (var calculation in calculations)
+            {
+                calculation.Operation = OperationEnum.Convert(calculation.Operation);
+            }
+            ViewBag.Calculations = calculations;
         }
     }
 }
