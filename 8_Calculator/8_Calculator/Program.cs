6e42b896e8220a065a1dba71d2a47fc6bb93bbf0
Quazzik <yuakorotkov@gmail.com>
2024-12-10 15:38:37 +0500

    added grafana and prometheus support

--- 8_Calculator/8_Calculator/Program.cs
+++ 8_Calculator/8_Calculator/Program.cs
@@ -1,7 +1,45 @@
+using OpenTelemetry.Metrics;
+using Prometheus;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddOpenTelemetry()
+    .WithMetrics(meterProviderBuilder =>
+    {
+    meterProviderBuilder.AddPrometheusExporter();
+
+    meterProviderBuilder.AddMeter("Microsoft.AspNetCore.Hosting",
+			"Microsoft.AspNetCore.Server.Kestrel");
+
+    // Status code 
+    meterProviderBuilder.AddMeter("Microsoft.AspNetCore.Http.Connections");
+
+        meterProviderBuilder.AddView("http.server.request.duration",
+            new ExplicitBucketHistogramConfiguration
+            {
+                Boundaries =
+                [
+                    0,
+                    0.005,
+                    0.01,
+                    0.025,
+                    0.05,
+                    0.075,
+                    0.1,
+                    0.25,
+                    0.5,
+                    0.75,
+                    1,
+                    2.5,
+                    5,
+                    7.5,
+                    10
+                ]
+            });
+    });
+
 
 var app = builder.Build();
 
@@ -13,11 +51,12 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+app.MapPrometheusScrapingEndpoint();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseEndpoints(endpoints => endpoints.MapMetrics());
 app.UseAuthorization();
 
 app.MapControllerRoute(
